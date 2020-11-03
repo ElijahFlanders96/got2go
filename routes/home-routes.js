383509f252b1ca681bf41dcda6d3bccdbdd7ff74
Raymond Gordon <rjgordon26@gmail.comgit config --global user.email rjgordon26@gmail.com>
2020-11-03 13:28:08 -0600

    adding connection file

---
+++ routes/home-routes.js
@@ -0,0 +1,16 @@
+let db = require("../models");
+
+module.exports = function(app) {
+      // POST route for saving a new review
+  app.post("/api/posts", function(req, res) {
+    console.log(req.body);
+    db.Post.create({
+      title: req.body.title,
+      body: req.body.body,
+      category: req.body.category
+    })
+      .then(function(dbPost) {
+        res.json(dbPost);
+      });
+  });
+}
