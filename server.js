8677406a58b78afcb97a10ac39d315a969d459ae
Elijah Flanders <elijahflanders15@gmail.com>
2020-11-04 11:09:13 -0600

    wrote the index.js file, playing around with plugin code on server.js file

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ let PORT = process.env.PORT || 8080;
 
 // Requiring our models for syncing
 let db = require("./models");
+let navigator = require("cordova");
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
@@ -20,6 +21,34 @@ app.use(express.static("public"));
 require("./routes/home-routes.js")(app);
 // require("./routes/api-routes.js")(app);
 // require("./routes/html-routes.js")(app);
+
+document.addEventListener("deviceready", onDeviceReady, false);
+    function onDeviceReady() {
+        console.log("navigator.geolocation works well");
+    }
+
+function getCurrentPosition() {
+  var onSuccess = function(position) {
+    console.log('Latitude: '          + position.coords.latitude          + '\n' +
+          'Longitude: '         + position.coords.longitude         + '\n' +
+          'Altitude: '          + position.coords.altitude          + '\n' +
+          'Accuracy: '          + position.coords.accuracy          + '\n' +
+          'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
+          'Heading: '           + position.coords.heading           + '\n' +
+          'Speed: '             + position.coords.speed             + '\n' +
+          'Timestamp: '         + position.timestamp                + '\n');
+};
+
+// onError Callback receives a PositionError object
+//
+function onError(error) {
+    console.log('code: '    + error.code    + '\n' +
+          'message: ' + error.message + '\n');
+}
+};
+
+navigator.geolocation.getCurrentPosition(onSuccess, onError);
+
 
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
