868d48b85644161d7a781c36314a517eaf3dbde4
Elijah Flanders <elijahflanders15@gmail.com>
2020-11-04 13:13:52 -0600

    required routes in server.js and plugged some geolocation code into home.js file

--- server.js
+++ server.js
@@ -19,35 +19,34 @@ app.use(express.static("public"));
 // Routes
 // =============================================================
 require("./routes/home-routes.js")(app);
-// require("./routes/api-routes.js")(app);
-// require("./routes/html-routes.js")(app);
+require("./routes/html-routes.js")(app);
 
-document.addEventListener("deviceready", onDeviceReady, false);
-    function onDeviceReady() {
-        console.log("navigator.geolocation works well");
-    }
+// document.addEventListener("deviceready", onDeviceReady, false);
+//     function onDeviceReady() {
+//         console.log("navigator.geolocation works well");
+//     }
 
-function getCurrentPosition() {
-  var onSuccess = function(position) {
-    console.log('Latitude: '          + position.coords.latitude          + '\n' +
-          'Longitude: '         + position.coords.longitude         + '\n' +
-          'Altitude: '          + position.coords.altitude          + '\n' +
-          'Accuracy: '          + position.coords.accuracy          + '\n' +
-          'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
-          'Heading: '           + position.coords.heading           + '\n' +
-          'Speed: '             + position.coords.speed             + '\n' +
-          'Timestamp: '         + position.timestamp                + '\n');
-};
+// function getCurrentPosition() {
+//   var onSuccess = function(position) {
+//     console.log('Latitude: '          + position.coords.latitude          + '\n' +
+//           'Longitude: '         + position.coords.longitude         + '\n' +
+//           'Altitude: '          + position.coords.altitude          + '\n' +
+//           'Accuracy: '          + position.coords.accuracy          + '\n' +
+//           'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
+//           'Heading: '           + position.coords.heading           + '\n' +
+//           'Speed: '             + position.coords.speed             + '\n' +
+//           'Timestamp: '         + position.timestamp                + '\n');
+// };
 
-// onError Callback receives a PositionError object
-//
-function onError(error) {
-    console.log('code: '    + error.code    + '\n' +
-          'message: ' + error.message + '\n');
-}
-};
+// // onError Callback receives a PositionError object
+// //
+// function onError(error) {
+//     console.log('code: '    + error.code    + '\n' +
+//           'message: ' + error.message + '\n');
+// }
+// };
 
-navigator.geolocation.getCurrentPosition(onSuccess, onError);
+// navigator.geolocation.getCurrentPosition(onSuccess, onError);
 
 
 // Syncing our sequelize models and then starting our Express app
