e01071a12a989e6fbcf4615cb2a82a77b03bc8c4
Raymond Gordon <rjgordon26@gmail.comgit config --global user.email rjgordon26@gmail.com>
2020-11-04 11:30:41 -0600

    adding route file updates

--- routes/home-routes.js
+++ routes/home-routes.js
@@ -1,16 +1,53 @@
 let db = require("../models");
 
 module.exports = function(app) {
-      // POST route for saving a new review
-  app.post("/api/posts", function(req, res) {
-    console.log(req.body);
-    db.Post.create({
-      title: req.body.title,
-      body: req.body.body,
-      category: req.body.category
+
+    // GET route for getting all of the reviews
+  app.get("/api/reviews/", function(req, res) {
+    db.Review.findAll({})
+      .then(function(dbReview) {
+        res.json(dbReview);
+      });
+  });
+
+  // Get route for retrieving a single review
+  app.get("/api/reviews/:id", function(req, res) {
+    db.Review.findOne({
+      where: {
+        id: req.params.id
+      }
     })
       .then(function(dbPost) {
         res.json(dbPost);
       });
   });
+
+  // POST route for saving a new review
+  app.post("/api/reviews", function(req, res) {
+    console.log(req.body);
+    db.Post.create({
+      type: req.body.type,
+      Purchase_necessary: req.body.Purchase_necessary,
+      cleanliness: req.body.cleanliness,
+      location: req.body.location,
+      Accessibility: req.body.Accessibility,
+      Amenities: req.body.Amenities,
+      Comments: req.body.Comments,
+    })
+      .then(function(dbPost) {
+        res.json(dbPost);
+      });
+  });
+    // PUT route for updating reviews
+    app.put("/api/reviews", function(req, res) {
+        db.Review.update(req.body,
+          {
+            where: {
+              id: req.body.id
+            }
+          })
+          .then(function(dbReview) {
+            res.json(dbReview);
+          });
+      });
 }
